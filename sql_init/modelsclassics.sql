a563ad81a65b28bab7eb057c0c769b6c1a483577
Narcís <nbuisac@users.noreply.github.com>
2026-02-08 21:29:18 +0100

    DROP TABLE modelsclassics
    S'han afegit comandes DROP TABLE per si volem reconstruir la base ja amb dades.

--- sql_init/modelsclassics.sql
+++ sql_init/modelsclassics.sql
@@ -24,6 +24,15 @@ CREATE DATABASE IF NOT EXISTS `modelsclassics`;
 USE `modelsclassics`;
 -- --------------------------------------------------------
 
+DROP TABLE IF EXISTS `orderdetails`;
+DROP TABLE IF EXISTS `payments`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `products`;
+DROP TABLE IF EXISTS `productlines`;
+DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `employees`;
+DROP TABLE IF EXISTS `offices`;
+
 --
 -- Estructura de tabla para la tabla `customers`
 --
